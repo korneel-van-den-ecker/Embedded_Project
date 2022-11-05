04f6d156e8c0f2235234e4c6e197793cbc266f9d
korneel-van-den-ecker <korneel_van_den_ecker@hotmail.com>
2022-11-05 13:00:47 +0000

    ok tafels keuze

--- PixelFrame/scripts/rekenScript.js
+++ PixelFrame/scripts/rekenScript.js
@@ -9,7 +9,7 @@ function kleur(r, g, b, bright) {
 
 
 var socket = io();
-
+//rekenen
 var opgaveAlsTekst = ""
 //0 = eerste getal
 //1 = plus of min
@@ -20,6 +20,7 @@ var opgave = []
 var opgaveGeschiedenis = []
 var maximumRekenenTot = 20
 
+//Raadsels
 // 0 = voor Na Of Tussen
 // 1 = midden
 // 2 = ingave 1 
@@ -29,7 +30,22 @@ var maximumRekenenTot = 20
 var raadselVoorNOpgave = []
 var raadselVoorNOpgaveGeschiedenis = []
 
+//Tafels
+
+var tafelspgave = []
+var tafelsOpgaveGeschiedenis = []
+var tafelVan = 2;
+var tafelTot = 5;
+var tafelMax = 10
+var tafelsKeuze = [true,true,false,false,false,false,false,false,false,false]
+
+
 $(document).ready(function () {
+
+    //initial State
+        $('#rekenModule').show();
+        $('#raadselModule').hide();        
+        $('#tafelModule').hide();
 
      //Navigatie Menu
      $('#rekenModuleLink').click(() => {
@@ -51,11 +67,152 @@ $(document).ready(function () {
         $('#rekenModule').hide();
         $('#raadselModule').hide();
         $('#tafelModule').show();
-        raadselBuilderVoorNa();
+        //raadselBuilderVoorNa();
         return false;
     })
 
-    /////////////////////:Tafeles///////////////////////
+    /////////////////////:Tafels///////////////////////
+
+    bepaalTafelVan = (tafels)=>{
+        //hoeveel tefels zijn er true?
+        var trueArray = [];
+        for (let index = 0; index < tafelMax; index++) {
+            if(tafels[index]){
+                trueArray.push(index)
+            }        
+        }
+        var keuzeRandom =  Math.floor(Math.random()*trueArray.length)
+        return trueArray[keuzeRandom] 
+    }
+
+    stelTafelOpgaveOp = () => {
+        tafelVan =  bepaalTafelVan(tafelsKeuze) +1
+        var maalOfGedeeld = Math.floor(Math.random() * 2) % 2
+        var a;
+        var b;
+        console.log(maalOfGedeeld)
+        if (maalOfGedeeld == 1) {
+            tafelspgave[1] = " X "
+            tafelspgave[0] = Math.floor(Math.random()*10)
+            tafelspgave[2] = tafelVan
+            tafelspgave[3] = tafelspgave[0] * tafelspgave[2]
+        } else {
+            tafelspgave[1] = " : "            
+            tafelspgave[0] = (Math.floor(Math.random()*10)+1) * tafelVan
+            tafelspgave[2] =  tafelVan
+            tafelspgave[3] =  tafelspgave[0] / tafelspgave[2]
+        }
+        console.log(tafelspgave)
+        $('#ingaveInputTafel').val("")
+        $('#tafel_opdracht').text(`${tafelspgave[0]} ${tafelspgave[1]} ${tafelspgave[2]} = `)
+
+    }
+
+    stelTafelOpgaveOp()
+
+    $('#tafelForm').submit((e) => {
+        e.preventDefault();
+
+        tafelspgave[4] = parseInt($('#ingaveInputTafel').val())
+        if (isNaN(tafelspgave[4])) {
+            window.alert("Geef een nummer in")
+        } else {
+            var resultaatTekst = ""
+            if (tafelspgave[4] == tafelspgave[3]) {
+                resultaatTekst = "Juist"
+                tekstKleur = new kleur(0, 0, 0, 1)
+                achertergrondkleur = new kleur(0, 255, 0, 1)
+            } else {
+                resultaatTekst = "Fout"
+                tekstKleur = new kleur(0, 0, 0, 1)
+                achertergrondkleur = new kleur(255, 0, 0, 1)
+            }
+            var teverzendenOject = {
+                "tekst": resultaatTekst,
+                "tekstKleur": tekstKleur,
+                "achertergrondkleur": achertergrondkleur,
+            }
+            socket.emit('PixelframeTekst', teverzendenOject);
+            function entry(result) {
+                return {
+                    "eersteFactor": tafelspgave[0],
+                    "plusOfMin": tafelspgave[1],
+                    "tweedeFactor": tafelspgave[2],
+                    "oplossing": tafelspgave[3],
+                    "ingave": tafelspgave[4],
+                    "eval": result
+                }
+            }
+            var entry1 = entry(resultaatTekst);
+            tafelsOpgaveGeschiedenis.push(entry1)
+            //console.log(opgaveGeschiedenis)
+            stelTafelOpgaveOp()
+            vulGeschiedenisTafelAan(entry1)
+        }
+    })
+
+    vulGeschiedenisTafelAan = (entry) => {
+        //$('#geschiedenisLijst').empty()        
+
+        if (entry.eval == "Juist") {
+            $('#tafelGeschiedenisLijst').prepend(
+                `<li class="list-group-item list-group-item-success">
+                ${entry.eersteFactor} ${entry.plusOfMin} ${entry.tweedeFactor} = ${entry.ingave}
+                </li>`)
+        }
+        else {
+            $('#tafelGeschiedenisLijst').prepend(
+                `<li class="list-group-item list-group-item-danger">
+                ${entry.eersteFactor} ${entry.plusOfMin} ${entry.tweedeFactor} = ${entry.ingave}
+                </li>`)
+        }
+        var aantalJuist = tafelsOpgaveGeschiedenis.filter(x => x.eval == "Juist").length
+        var totaalGemaakt = tafelsOpgaveGeschiedenis.length
+        var score = `Score: ${aantalJuist} / ${totaalGemaakt}`
+        $('#totaalTafel').html(score)
+
+    }
+
+    //Selectie Welke tafels 
+    stelKeuzeMenuOp = () =>{
+        
+
+        for (let index = 0; index < tafelMax; index++) {
+            tafel = tafelsKeuze[index]
+            if(tafel){
+                $('#tafelsSelectie').append(
+                    `<label class="btn btn-secondary active tafelsInput">
+                        <input  type="checkbox" name="options" id="${index+1}" autocomplete="off" checked> ${index+1}
+                    </label>`
+                    )
+            }
+            else{
+                $('#tafelsSelectie').append(
+                    `<label class="btn btn-secondary tafelsInput">
+                        <input  type="checkbox" name="options" id="${index+1}" autocomplete="off"> ${index+1}
+                    </label>`
+                    )
+            }
+           
+        }
+    }
+
+    updateKeuzeMenu = (index,status)=>{
+        $('#'+index).checked = status;
+    }
+    
+    stelKeuzeMenuOp()
+    
+    $('.tafelsInput').click((event)=>{
+        var idTafelKnop = event.target.children[0].id -1
+        var status = event.target.children[0].checked
+
+        $('#'+idTafelKnop).checked = status
+        tafelsKeuze[idTafelKnop] = !tafelsKeuze[idTafelKnop]
+        stelTafelOpgaveOp()
+    })
+    
+
 
     /////////////////////:REKENEN//////////////////////
 
@@ -63,10 +220,10 @@ $(document).ready(function () {
         var plusOfMin = Math.floor(Math.random() * 2) % 2
         var a;
         var b;
+        opgave[0] = Math.floor(Math.random() * (maximumRekenenTot + 1))
         console.log(plusOfMin)
         if (plusOfMin == 1) {
             opgave[1] = "-"
-            opgave[0] = Math.floor(Math.random() * (maximumRekenenTot + 1))
             opgave[2] = Math.floor(Math.random() * (opgave[0] + 1))
             opgave[3] = opgave[0] - opgave[2]
         } else {
